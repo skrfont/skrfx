51d078b98d85e4180680f0160afe05ab5fafe230
sakuramo <sakuramo@tencent.com>
2019-07-23 20:30:42 +0800

    stateManage, rxBlockBox

--- src/index.js
+++ src/index.js
@@ -1,5 +1,61 @@
-import * as Rx from 'rxjs';
+import { forIn } from 'lodash';
+import { BehaviorSubject } from 'rxjs';
 
-export default function () {
-  console.log('Hello World!', Rx);
+export function rxBlackBox(repoCfg = {}, handle = () => { }, exportList = []) {
+  return function BlackBox() {
+    const ctx = {};
+    forIn(repoCfg, (value, key) => {
+      if (key === 'events') {
+        return;
+      }
+      ctx[key] = new BehaviorSubject(value);
+    });
+    const events = repoCfg.events ? repoCfg.events : this;
+    handle(events, ctx);
+    const output = {};
+    exportList.forEach((v) => {
+      output[v] = ctx[v];
+    });
+    return output;
+  };
 }
+
+class StateManage {
+  constructor(rules, init) {
+    this.state = init;
+    this.rules = rules;
+  }
+
+  Action(event) {
+    // 锁定状态、异步状态、异常状态
+    const handerList = this.rules[this.state];
+    if (!handerList) {
+      return this;
+    }
+    const hander = handerList[event];
+    if (!hander) {
+      return this;
+    }
+    const {
+      state: lockState = this.state,
+      af: asyncFn = () => lockState,
+      ef: errorFn = () => { },
+    } = hander;
+    this.state = lockState;
+    asyncFn(lockState).then((s) => {
+      this.state = s;
+    }).catch(e => errorFn(e));
+    return this;
+  }
+}
+
+export function stateManage({ list, init }) {
+  return new StateManage(list, init);
+}
+
+const Skrfx = {
+  rxBlackBox,
+  stateManage,
+};
+
+export default Skrfx;
